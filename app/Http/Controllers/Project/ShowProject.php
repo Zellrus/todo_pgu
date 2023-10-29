8ade7125a8ef2e1227da2163f564845f40c20a63
Zellrus <alesha-shvec@mail.ru>
2023-10-29 16:53:23 +0200

    savefiles

---
+++ app/Http/Controllers/Project/ShowProject.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Project;
+
+use App\Http\Controllers\Controller;
+
+class ShowProjectController extends Controller
+{
+    public function index()
+    {
+
+    }
+}
