e95686e5fcd6d1cc4d75317a9b0f7bd9d3e3df03
Zellrus <alesha-shvec@mail.ru>
2023-12-26 21:40:32 +0200

    fix Project Show Controller

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -5,6 +5,8 @@ namespace App\Exceptions;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
+
+
 class Handler extends ExceptionHandler
 {
     /**
@@ -24,7 +26,10 @@ class Handler extends ExceptionHandler
     public function register(): void
     {
         $this->reportable(function (Throwable $e) {
-            //
+
         });
+
+
     }
+
 }
